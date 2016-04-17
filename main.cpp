81896a5961bbcaea488b0b22fd85163abb541506
InWiLL <yk652329709@gmail.com>
2016-04-17 16:27:29 +0800

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -1,46 +1,68 @@
 #define REP(i,n,m) for(int i=(n);i<(m);i++)
 
 #include<iostream>
+#include<vector>
 #include<cstring>
 using namespace std;
 
 int dect[24][6]={{2,1,5,0,4,3},{2,0,1,4,5,3},{2,4,0,5,1,3},{2,5,4,1,0,3},{4,2,5,0,3,1},{5,2,1,4,3,0},
 					   {1,2,0,5,3,4},{0,2,4,1,3,5},{0,1,2,3,4,5},{4,0,2,3,5,1},{5,4,2,3,1,0},{1,5,2,3,0,4},
-					   {5,1,3,2,4,0},{1,0,3,2,5,4},{0,4,3,2,1,5},{4,5,3,2,0,1},{1,3,5,0,2,4},{0,3,1,4,2,5},
-					   {4,3,0,5,2,1},{5,3,4,1,2,0},{5,1,3,2,4,0},{1,0,3,2,5,4},{0,4,3,2,1,5},{4,5,3,2,0,1}};
-string cube[4][24][6];
-int n,ans,num;
+					   {5,1,3,2,4,0},{1,0,3,2,5,4},{0,4,3,2,1,5},{4,5,3,2,0,1},{3,4,5,0,1,2},{3,5,1,4,0,2},
+					   {3,1,0,5,4,2},{3,0,4,1,5,2},{1,3,5,0,2,4},{0,3,1,4,2,5},{4,3,0,5,2,1},{5,3,4,1,2,0}};
+int n,ans,tot,num,r[4],cube[4][6],color[4][6];
 
-int compare(string *T,string *P)
+vector<string> names;
+int ID(string s)
 {
-	int N=0;
+	int n=names.size();
+	REP(i,0,n)
+		if(names[i]==s) return i;
+	names.push_back(s);
+	return n;
+}
+
+void check()
+{
+	REP(i,0,4) REP(j,0,6)
+		color[i][j]=cube[i][dect[r[i]][j]];
+	int tot=0;
 	REP(i,0,6)
-		if(T[i]!=P[i])
-			N++;
-	return N;
+	{
+		int cnt[24];
+		memset(cnt,0,sizeof(cnt));
+		int maxface=0;
+		REP(j,0,n)
+			maxface=max(maxface,++cnt[color[j][i]]);
+		tot+=n-maxface;
+	}
+	ans=min(ans,tot);
+}
+
+void dfs(int d)
+{
+	if(d==n) check();
+	else REP(i,0,24)
+	{
+		r[d]=i;
+		dfs(d+1);
+	}
 }
 
 int main()
 {
 	while(cin>>n&&n!=0)
 	{
-		string C[6];
+		names.clear();
 		REP(i,0,n)
-		{
-			REP(j,0,6)//可以优化
-				cin>>C[j];
-			REP(j,0,24)
-				REP(k,0,6)
-					cube[i][j][k]=C[dect[j][k]];
-		}
-		ans=0;
-		REP(i,1,n)
-		{
-			num=6;
-			REP(j,0,24)
-				num=min(num,compare(cube[0][0],cube[i][j]));
-			ans+=num;
-		}
+			REP(j,0,6)
+			{
+				string s;
+				cin>>s;
+				cube[i][j]=ID(s);
+			}
+		ans=n*6;
+		r[0]=0;
+		dfs(1);
 		cout<<ans<<endl;
 	}
 	return 0;
